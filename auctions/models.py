c96b7692fea6db762944c14734c8077319773b25
ninjoshATTACK <joshua.gatpandan@gmail.com>
2022-10-12 23:38:13 -0800

    performed migrations and added some models

--- auctions/models.py
+++ auctions/models.py
@@ -14,10 +14,20 @@ class Listing(models.Model):
     def __str__(self):
         return f'{self.title}'
 
-# model for bids
+# model for bids  [NOT CORRECT]
 class Bid(models.Model):
+    title = models.CharField(max_length=64)
+    desc = models.CharField(max_length=100)
+    startbid = models.ImageField()
 
+    def __str__(self):
+        return f'{self.title}'
 
-# model for comments
+# model for comments  [NOT CORRECT]
 class Comment(models.Model):
-    pass
+    title = models.CharField(max_length=64)
+    desc = models.CharField(max_length=100)
+    startbid = models.ImageField()
+
+    def __str__(self):
+        return f'{self.title}'
